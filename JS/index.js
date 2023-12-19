ba3a1977fd733cb90b0e84b2bd10d4dfc66c97d9
Ali Sadeghi <ali909392@gmail.com>
2023-12-19 22:18:34 +0330

    Add base structure of page

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,43 @@
 "use strict";
 console.log("HELLO");
+const input = document.querySelector(".input-list");
+const output = document.querySelector(".output-list");
+input.value = `آپدیت صف #وب 👇🏻
+
+1- فاطمه رضایی @Rezaie_f98 
+2- فرشاد دولت‌یاری @Farshad_80_1 
+3- شایان رجبی @Shayan_rajaby
+4- ریحانه روحی @reyh_an
+5- علی قاسم‌پور @AliAkbar00100
+6- ارشیا مردانی @mozadvaj
+7- سعید محمدی @saeed7797
+8- سارا محمدی @pciou
+9- ابولفضل حصارکی @Abolfazl_hsr
+10- مبینا وجدی @M0bio
+11- الیاس اسماعیلی @elias_esm
+12-  آرش ثانی @Arashsani
+13- محمد شفیعی @mshafiei1
+14-  امیرحسین بشیری @Amirhosseinshonam
+15- مبین صالحی @Mwbi_slh
+16-  آریا شریف @TSN963
+17- فاطمه امینی @Fatemeh_amini13
+18- محدثه قهرمانی @M_ghahremaniii7
+19- معصومه دادبخش @Masoomeh_dadbakhsh
+20- علیرضا فتحی @ItsAlirezzz
+21-  آیدا پیمائی @aida_ap7
+22- محمد چنانی @developer_codee
+23-  آرزو روحی @Wish1375
+24- مهیار سعدی @There_is_no_user
+25- مهدی ملکی @M_maleki1370
+26- مرضیه بیرانوند @MGityB
+27- علی رفیعی @ART91128LJ
+28-  سپهر ملکی @smplo
+29-  اسحاق ترابی @kafipasargad
+30- عرفان اکبری @Erfan25974
+31- محدثه شیخ‌ اویسی @Mohisho
+32- حسام مظلوم @hesam_0101
+33- امیرحسین ارندان @AmirhoseinArandan
+
+#وب`;
+output.value = input.value;
 //# sourceMappingURL=index.js.map
