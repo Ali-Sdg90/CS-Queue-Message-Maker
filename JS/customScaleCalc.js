832f9d59cdbabfbaa1ff323d6f88c976daf30558
Ali Sadeghi <ali909392@gmail.com>
2024-01-17 17:55:46 +0330

    My customScaleCalc now listen to page resizes

--- JS/customScaleCalc.js
+++ JS/customScaleCalc.js
@@ -1,13 +1,21 @@
 "use strict";
 const customScaleDiv = document.querySelector(".custom-scale-div");
-const scaleWarning = document.querySelector(".scale-warning");
-const pageWidth = customScaleDiv.scrollWidth;
-const pageHeight = customScaleDiv.scrollHeight;
-if (pageHeight > pageWidth) {
-    console.log(">>", pageHeight);
-    console.log("Mobile View");
-    customScaleDiv.classList.add("custom-scale-calc");
-    let scale = 1 - ((pageHeight / pageWidth) * 0.5) / (900 / 477);
-    customScaleDiv.style.transform = `scale(${scale}) rotate(90deg)`;
-}
+const customScaleCalc = () => {
+    const pageWidth = window.innerWidth;
+    const pageHeight = window.innerHeight;
+    if (pageWidth > 270) {
+        if (pageHeight > pageWidth) {
+            console.log(pageWidth, pageHeight);
+            customScaleDiv.classList.add("custom-scale-calc");
+            let scale = 1 - ((pageHeight / pageWidth) * 0.5) / (900 / 477);
+            customScaleDiv.style.transform = `scale(${scale}) rotate(90deg)`;
+        }
+        else {
+            customScaleDiv.classList.remove("custom-scale-calc");
+            customScaleDiv.style.transform = "";
+        }
+    }
+};
+customScaleCalc();
+window.addEventListener("resize", customScaleCalc);
 //# sourceMappingURL=customScaleCalc.js.map
--- TS/customScaleCalc.ts
+++ TS/customScaleCalc.ts
@@ -1,21 +1,29 @@
 const customScaleDiv = document.querySelector(
     ".custom-scale-div"
 ) as HTMLElement;
-const scaleWarning = document.querySelector(".scale-warning") as HTMLElement;
 
-const pageWidth = customScaleDiv.scrollWidth;
-const pageHeight = customScaleDiv.scrollHeight;
+const customScaleCalc = () => {
+    const pageWidth = window.innerWidth;
+    const pageHeight = window.innerHeight;
 
-if (pageHeight > pageWidth) {
-    console.log(">>", pageHeight);
-    console.log("Mobile View");
+    if (pageWidth > 270) {
+        if (pageHeight > pageWidth) {
+            console.log(pageWidth, pageHeight);
 
-    customScaleDiv.classList.add("custom-scale-calc");
+            customScaleDiv.classList.add("custom-scale-calc");
 
-    let scale = 1 - ((pageHeight / pageWidth) * 0.5) / (900 / 477);
+            let scale = 1 - ((pageHeight / pageWidth) * 0.5) / (900 / 477);
 
-    customScaleDiv.style.transform = `scale(${scale}) rotate(90deg)`;
-}
+            customScaleDiv.style.transform = `scale(${scale}) rotate(90deg)`;
+        } else {
+            customScaleDiv.classList.remove("custom-scale-calc");
+            customScaleDiv.style.transform = "";
+        }
+    }
+};
+
+customScaleCalc();
+window.addEventListener("resize", customScaleCalc);
 
 // Description: This is a method I created myself, and I find it very
 // interesting. I named it customScaleCalc. What it does is, when the
@@ -24,5 +32,4 @@ if (pageHeight > pageWidth) {
 // content with a certain ratio. Ultimately, in the mobile device's
 // browser,the application is correctly displayed with a 90-degree
 // rotation, allowing devices with different sizes to use the program.
-// This calculation of how to display the screen is performed with the
-// initial page load.
+// This calculation of how to display the screen is performed.
