b190bda5bd25086da676316c792f088f10c03123
Ali Sadeghi <ali909392@gmail.com>
2024-01-21 12:05:56 +0330

    Improve styles of EditList / Update demoList / Start work on new queue structure

--- JS/demoList.js
+++ JS/demoList.js
@@ -1,15 +1,19 @@
 "use strict";
 const addDemoList = () => {
-    input.value = `آپدیت صف #سی_شارپ 👇🏻
+    input.value = ` آپدیت صف #سی_شارپ 👇🏻
 
-1- محمد عبدلی @M06061
-2- پرهام حسنی @parhamHasani
-3- مهدی بیگدلی @Mhdi_bigdely
-4- پارسا ستوده @parsa_set
-5- گیتا وجدانی @GitaVejdani
-6- مرضیه فتحی زاده @Miss_ftz
-7- امیرحسین اعرابی @amirhossein_wp_goalie
+1- علی صادقی @Ali_Sdg90
+2- حسام حسنی @HessamHasaniWP
+3- مهدی حسامی @Mhdi_Hessami
+4- حسام ستوده @Hessam_set
+5- گیتا حسامی @GitaHessami
+6- مرضیه حسامی زاده @Miss_HessamZ
+7- امیرحسین حسامی @amirhossein_wp_Hessam
 
-#سی_شارپ`;
+#سی_شارپ
+
+
+برنامه مدریت صف برای مسئول صف:
+https://ali-sdg90.github.io/CS-Queue-Message-Maker/`;
 };
 //# sourceMappingURL=demoList.js.map
